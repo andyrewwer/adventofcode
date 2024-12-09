efadd45a5c9afbb5b029298a71c3e5fc4ede1adb
Andrew Weeks <andrew.weeks@benchmarkanalytics.com>
2024-12-08 18:42:01 -0600

    Working sol/n

--- 2024/day7/python.py
+++ 2024/day7/python.py
@@ -1,14 +1,32 @@
 import sys
+import time
 
 file_name = 'input.csv' if len(sys.argv) < 2 else 'input-basic.csv'
+with open(file_name, "r") as file:
+  input_data = file.readlines()
 
+def calculate(nums):
+    if len(nums) == 1:
+        return nums
 
-def process_part1(row):
-    sum, nums = row.split(":")
-    nums = nums.strip().split()
+    return (calculate([nums[0] + nums[1]] + nums[2:]) +  # add
+          calculate([nums[0] * nums[1]] + nums[2:]) + # multiply
+          calculate([int(str(nums[0]) + str(nums[1]))] + nums[2:])
+          )   
 
-with open(file_name) as file:
-    sum_1 = 0
-    
-    for i, row in enumerate(file):
-        sum_1 += process_part1(row)
+# read input_data from file
+
+total = 0
+for line in input_data:
+    left, right = line.split(": ")
+    result = int(left)
+    operands = list(map(int, right.split()))
+
+    # get results of all possible operand/operator combinations
+    results = calculate(operands)
+
+    # if one of the possible results is correct
+    if result in results:
+    total += result  # add it to the total
+
+print(total)
